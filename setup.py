8ad07b2e25b924326861ea02450261ba2b368dc3
Gabriel Curio <g.curio@gmail.com>
2017-12-08 08:44:16 -0600

    add setup files.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='inotifier',
+    version='0.1',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Filesystems',
+        'Topic :: System :: Operating System Kernels :: Linux',
+        'Programming Language :: Python :: 2'
+    ],
+    packages=['tests', 'inotifier'],
+    url='https://github.com/GabrielC101/filer',
+    license='Apache2',
+    author='Gabriel Curio',
+    extras_requirements={'examples':
+                             ['inotifier.examples',
+                              'inotifier.examples.printer',
+                              'inotifier.examples.renamer'
+                              ]
+                         },
+    description='Inotify Event Handler'
+)
