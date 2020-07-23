49cec2e51952c6257f7b4a1786d0ec5ba793b1dd
Gabriel Curio <g.curio@gmail.com>
2020-07-23 09:31:05 -0500

    remove stray print statement.

--- inotifier/inotifier.py
+++ inotifier/inotifier.py
@@ -20,7 +20,6 @@ class InotifierBase:
         self.i.add_watch(self.initial_watch_path)
         try:
             for base_event in self.i.event_gen():
-                print(f'base event {base_event}')
                 if base_event is not None:
                     inotify_event = InotifyEvent.build_from_inotify_adapter(
                         base_event
